1c8b1abaadbb201ce5637ad4486323c81ae47ef7
M Surya Narayana <mallasurya189489@gmail.com>
2026-03-31 05:59:24 +0530

    fix static issue

--- msme-repo-main/frontend/vite.config.js
+++ msme-repo-main/frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/static/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
